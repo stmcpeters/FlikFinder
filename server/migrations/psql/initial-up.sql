7316a97bd59a801e84f44e01d651191c23d4a616
stmcpeters <stmcpeters@gmail.com>
2024-10-16 15:46:57 -0700

    edits psql file to only have create table query

--- server/migrations/psql/initial-up.sql
+++ server/migrations/psql/initial-up.sql
@@ -1,24 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.13 (Homebrew)
--- Dumped by pg_dump version 14.13 (Homebrew)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
 
 --
 -- Name: genres; Type: TABLE; Schema: public; Owner: beigeh0ney
@@ -30,8 +9,6 @@ CREATE TABLE public.genres (
 );
 
 
-ALTER TABLE public.genres OWNER TO beigeh0ney;
-
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: beigeh0ney
 --
@@ -41,37 +18,6 @@ CREATE TABLE public.users (
     username character varying NOT NULL,
     email character varying NOT NULL
 );
-
-
-ALTER TABLE public.users OWNER TO beigeh0ney;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: beigeh0ney
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.users_id_seq OWNER TO beigeh0ney;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: beigeh0ney
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
@@ -119,47 +65,4 @@ COPY public.users (id, username, email) FROM stdin;
 SELECT pg_catalog.setval('public.users_id_seq', 1, true);
 
 
---
--- Name: genres genres_genre_name_key; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.genres
-    ADD CONSTRAINT genres_genre_name_key UNIQUE (genre_name);
-
-
---
--- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.genres
-    ADD CONSTRAINT genres_pkey PRIMARY KEY (genre_id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
---
--- PostgreSQL database dump complete
---
 

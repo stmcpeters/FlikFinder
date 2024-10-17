0e04b724cb72eb15942d105ce8ef41a29084dd10
stmcpeters <stmcpeters@gmail.com>
2024-10-17 12:35:27 -0700

    adds dropdown skeleton to select genres component

--- client/src/components/SelectGenres.jsx
+++ client/src/components/SelectGenres.jsx
@@ -1,2 +1,22 @@
 // toggleGenre() => adds or removes genres from the selectedGenres
 // saveGenres() => saves the user’s selected genres to DB
+import React from 'react'
+import Select from 'react-select'
+
+export default function SelectGenres() {
+
+  // populate options with genres in genres table
+  const options = [
+    { value: 'horror', label: 'Horror'},
+    { value: 'thriller', label: 'Thriller'}
+  ]
+
+
+
+  return (
+    <>
+      <h2>Select Genres</h2>
+      <Select options={options} isMulti/>
+    </>
+  )
+}

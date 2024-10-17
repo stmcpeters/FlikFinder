6f3d5c5dd47d6b9a942f09f7fd034c2d1f8b367a
stmcpeters <stmcpeters@gmail.com>
2024-10-17 15:30:07 -0700

    updates pg_dump file

--- server/db.sql
+++ server/db.sql
@@ -33,6 +33,43 @@ CREATE TABLE public.genres (
 ALTER TABLE public.genres OWNER TO beigeh0ney;
 
 --
+-- Name: reviews; Type: TABLE; Schema: public; Owner: beigeh0ney
+--
+
+CREATE TABLE public.reviews (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    movie_id character varying NOT NULL,
+    review_body text NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.reviews OWNER TO beigeh0ney;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE; Schema: public; Owner: beigeh0ney
+--
+
+CREATE SEQUENCE public.reviews_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reviews_id_seq OWNER TO beigeh0ney;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: beigeh0ney
+--
+
+ALTER SEQUENCE public.reviews_id_seq OWNED BY public.reviews.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: beigeh0ney
 --
 
@@ -68,6 +105,13 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: reviews id; Type: DEFAULT; Schema: public; Owner: beigeh0ney
+--
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN id SET DEFAULT nextval('public.reviews_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: beigeh0ney
 --
 
@@ -79,27 +123,37 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 --
 
 COPY public.genres (genre_id, genre_name) FROM stdin;
-action	Action
-adventure	Adventure
-animation	Animation
-comedy	Comedy
-crime	Crime
-documentary	Documentary
-drama	Drama
-family	Family
-fantasy	Fantasy
-history	History
-horror	Horror
-music	Music
-mystery	Mystery
-news	News
-reality	Reality
-romance	Romance
-scifi	Science Fiction
-talk	Talk Show
-thriller	Thriller
-war	War
-western	Western
+action  Action
+adventure       Adventure
+animation       Animation
+comedy  Comedy
+crime   Crime
+documentary     Documentary
+drama   Drama
+family  Family
+fantasy Fantasy
+history History
+horror  Horror
+music   Music
+mystery Mystery
+news    News
+reality Reality
+romance Romance
+scifi   Science Fiction
+talk    Talk Show
+thriller        Thriller
+war     War
+western Western
+\.
+
+
+--
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: beigeh0ney
+--
+
+COPY public.reviews (id, user_id, movie_id, review_body, created_at) FROM stdin;
+1       2       56608   #Captured dives into the dark side of the human psyche, blending horror and thriller elements effectively. The film follows a group of friends who find themselves ensnared in a web of fear and paranoia after an ill-fated adventure. Lizze Gordon delivers a standout performance, capturing the raw emotion and terror of her character. While the plot has some predictable twists, the intense atmosphere and chilling cinematography keep viewers engaged. Fans of the genre may find it a worthwhile watch, especially for its tension and suspenseful moments.   2024-10-17 14:08:55.905999
+2       1       56608   Despite its intriguing premise, #Captured ultimately falls short of its potential. The film starts strong, setting up a gripping scenario that promises thrills and chills. However, as the story unfolds, the pacing drags, and character development is lacking. While some performances are commendable, particularly by Cody Renee Cameron, the script fails to deliver memorable dialogue or compelling motivations. Horror enthusiasts may appreciate a few jump scares, but overall, it feels like a missed opportunity to explore deeper themes of captivity and survival.        2024-10-17 14:10:08.834332
 \.
 
 
@@ -108,15 +162,23 @@ western	Western
 --
 
 COPY public.users (id, username, email) FROM stdin;
-1	steph	steph@gmail.com
+1       steph   steph@gmail.com
+2       cinephile415    movies@gmail.com
 \.
+
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: beigeh0ney
+--
+
+SELECT pg_catalog.setval('public.reviews_id_seq', 2, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: beigeh0ney
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+SELECT pg_catalog.setval('public.users_id_seq', 2, true);
 
 
 --
@@ -133,6 +195,14 @@ ALTER TABLE ONLY public.genres
 
 ALTER TABLE ONLY public.genres
     ADD CONSTRAINT genres_pkey PRIMARY KEY (genre_id);
+
+
+--
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: beigeh0ney
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
 
 
 --
@@ -160,6 +230,13 @@ ALTER TABLE ONLY public.users
 
 
 --
--- PostgreSQL database dump complete
+-- Name: reviews reviews_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: beigeh0ney
 --
 
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--

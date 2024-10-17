100c256514fd8800b8c619fd4f41cfa09cd41b1d
stmcpeters <stmcpeters@gmail.com>
2024-10-17 13:36:32 -0700

    creates navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+export default function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">FlixFinder</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">About Us</Nav.Link>
+          </Nav>
+          <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Guest</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  )
+}

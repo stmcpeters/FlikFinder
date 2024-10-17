adfbd3261ad901b71970b0469db4bf7eadee7df9
stmcpeters <stmcpeters@gmail.com>
2024-10-17 12:35:51 -0700

    imports select genres component to render to page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserAuth from './components/UserAuth.jsx'
+import SelectGenres from './components/SelectGenres.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
       </nav>
 
       <UserAuth />
+      <SelectGenres />
     </>
   )
 }

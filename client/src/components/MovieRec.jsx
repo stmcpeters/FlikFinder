3768d0004e7e09901040f683842290ca8dbc94e1
stmcpeters <stmcpeters@gmail.com>
2024-10-17 12:46:41 -0700

    adds h2 to component to display

--- client/src/components/MovieRec.jsx
+++ client/src/components/MovieRec.jsx
@@ -1,3 +1,13 @@
 //  fetchRecommendation() => retrieves array of movie recommendation based on user’s selected genre
 //  selectRandomMovie(movies) => takes the array of movies recommended and uses math.random to select and return a random single movie
 //  displayMovie(movie) => renders the recommended movie’s data to the screen
+import React from 'react'
+
+export default function MovieRec() {
+  return (
+    <>
+      <h2>Movie Rec</h2>
+
+    </>
+  )
+}

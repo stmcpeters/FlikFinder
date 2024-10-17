8355d4a711d8a2571fdc3107ae8576ec462a0197
stmcpeters <stmcpeters@gmail.com>
2024-10-17 12:47:13 -0700

    adds h2 to component to display

--- client/src/components/MovieReview.jsx
+++ client/src/components/MovieReview.jsx
@@ -1,2 +1,11 @@
 // submitReview() =>saves a user’s review for a specific movie to the DB
 // userReviews() => fetches all user reviews for specific movie using its movieID
+import React from 'react'
+
+export default function MovieReview() {
+  return (
+    <>
+      <h2>Movie Review</h2>
+    </>
+  )
+}

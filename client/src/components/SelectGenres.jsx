989c89cc5862bff91d463dd0aedb8b1e206c44a7
stmcpeters <stmcpeters@gmail.com>
2024-10-17 16:17:38 -0700

    hard codes genre data for selection component

--- client/src/components/SelectGenres.jsx
+++ client/src/components/SelectGenres.jsx
@@ -5,10 +5,28 @@ import Select from 'react-select'
 
 export default function SelectGenres() {
 
-  // populate options with genres in genres table
+  // // populate options with genres in genres table
   const options = [
-    { value: 'horror', label: 'Horror'},
-    { value: 'thriller', label: 'Thriller'}
+    { value: "action", label: "Action" },
+    { value: "adventure", label: "Adventure" },
+    { value: "animation", label: "Animation" },
+    { value: "crime", label: "Crime" },
+    { value: "documentary", label: "Documentary" },
+    { value: "drama", label: "Drama" },
+    { value: "family", label: "Family"},
+    { value: "fantasy", label: "Fantasy" },
+    { value: "history", label: "History" },
+    { value: "horror", label: "Horror" },
+    { value: "music", label: "Music" },
+    { value: "mystery", label: "Mystery" },
+    { value: "news", label: "News" },
+    { value: "reality", label: "Reality" },
+    { value: "romance", label: "Romance" },
+    { value: "scifi", label: "Science Fiction" },
+    { value: "talk", label: "Talk Show" },
+    { value: "thriller", label: "Thriller" },
+    { value: "war", label: "War" },
+    { value: "western", label: "Western" }
   ]
 
 

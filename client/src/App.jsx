2401e4102b17f252f49891426d9d7f0459ee92b2
stmcpeters <stmcpeters@gmail.com>
2024-10-17 13:39:07 -0700

    removes old navbar and imports new navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar.jsx'
 import UserAuth from './components/UserAuth.jsx'
 import SelectGenres from './components/SelectGenres.jsx'
 import MovieRec from './components/MovieRec.jsx'
@@ -9,22 +9,71 @@ import MovieReview from './components/MovieReview.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  // initialize and update users state
+  const [users, setUsers] = useState([]);
+  // initialize and update genres state
+  const [genres, setGenres] = useState([]);
+  // initialize and update reviews state
+  const [reviews, setReviews] = useState([]);
+
+  // fetching users from database
+  const loadUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:5001/db/users");
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error('Error fetching users: ', error);
+    }
+  }
+
+  // fetches users from database on page render
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+
+  // fetches genres from database
+  const loadGenres = async () => {
+    try {
+      const response = fetch("http://localhost:5001/db/genres");
+      const data = await response.json();
+      setGenres(data);
+    } catch (error) {
+      console.error('Error fetching genres: ', error);
+    }
+  }
+
+  // fetches genres from database on page render
+  useEffect(() => {
+    loadGenres();
+  }, []);
+
+  // fetches reviews from database
+  const loadReviews = async () => {
+    try {
+      const response = fetch("http://localhost:5001/db/reviews");
+      const data = await response.json();
+      setReviews(data);
+    } catch (error) {
+      console.error('Error fetching reviews: ', error);
+    }
+  }
+
+    // fetches reviews from database on page render
+    useEffect(() => {
+      loadReviews();
+    }, []);
+
+
 
   return (
     <>
-      <nav>
-        <h1>Logo</h1>
-        <ul className='nav-items'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Login/Sign Up</a></li>
-          <li>Signed in as: <a href="#">User</a></li>
-        </ul>
-      </nav>
-
+      <NavBar />
       <UserAuth />
-      <SelectGenres />
+      <SelectGenres genres={genres} />
       <MovieRec />
-      <MovieReview />
+      <MovieReview reviews={reviews} />
     </>
   )
 }

4f2da577879a1dcc25a5e74e2e6095624639e137
stmcpeters <stmcpeters@gmail.com>
2024-10-19 10:11:43 -0700

    fixes conditional rendering so movie details are only displayed after movies array has been loaded

--- client/src/components/MovieRec.jsx
+++ client/src/components/MovieRec.jsx
@@ -1,25 +1,44 @@
-//  fetchRecommendation() => retrieves array of movie recommendation based on user’s selected genre
-//  selectRandomMovie(movies) => takes the array of movies recommended and uses math.random to select and return a random single movie
-//  displayMovie(movie) => renders the recommended movie’s data to the screen
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export default function MovieRec({ movies }) {
+
+  //  fetchRecommendation() => retrieves array of movie recommendation based on user’s selected genre
+//  selectRandomMovie(movies) => takes the array of movies recommended and uses math.random to select and return a random single movie
+//  displayMovie(movie) => renders the recommended movie’s data to the screen
+
+  const [randomMovie, setRandomMovie] = useState(null);
 
   const selectRandomMovie = (movies) => {
     const randomIndex = Math.floor(Math.random() * movies.length);
     return movies[randomIndex];
   }
 
-  const randomMovie = selectRandomMovie(movies);
+  // useEffect is activated every time the movies array changes, checking that movies have loaded. once the movies array is loaded, selectRandomMovie() is called and updates and sets the state of randomMovie to 1 random movie chosen out of the movies array
+  useEffect(() => {
+    if(movies.length > 0) {
+      setRandomMovie(selectRandomMovie(movies));
+      }
+    }, [movies]);
+
+
+    // if movies array has not loaded, loading message will be shown
+    if(!movies.length) {
+      return <p>Loading...</p>
+    }
 
   //console.log('Movies: ', movies);
 
   return (
     <div className="movie-rec">
       <h2>Movie Rec</h2>
-      {/* doesn't load on page render but once rendered commented out section displays movie details of a random movie */}
-      {/* <h3>{randomMovie.title} ({randomMovie.releaseYear})</h3>
-      <p>{randomMovie.overview}</p> */}
+      {/* conditionally shows movie details only if randomMovie has been set (is truthy) */}
+      {randomMovie && (
+        <>
+          <h3>{randomMovie.title} ({randomMovie.releaseYear})</h3>
+          <p>{randomMovie.overview}</p>
+        </>
+      )}
     </div>
   )
 }

6a73b5deed552a3a1c14c299c59976575ea1824e
stmcpeters <stmcpeters@gmail.com>
2024-10-23 13:37:42 -0700

    styles auth page

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,12 +1,16 @@
 import UserAuth from "../components/UserAuth";
-import NavBar from "../components/NavBar";
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import CardBody from "react-bootstrap/esm/CardBody";
 
 export default function Auth() {
   return (
-    <>
-      <NavBar />
-      <UserAuth />
-    </>
+    <div className="auth-card">
+      <Card>
+        <CardBody>
+          <UserAuth />
+        </CardBody>
+      </Card>
+    </div>
   )
 }

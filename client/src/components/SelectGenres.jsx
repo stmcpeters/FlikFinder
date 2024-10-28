a5bd45bd999a9ff3f8404b5dd9101a032f4bf7bd
stmcpeters <stmcpeters@gmail.com>
2024-10-27 20:35:53 -0700

    uses hardcoded genre data to populate select element; added comments for troubleshooting using genre DB data

--- client/src/components/SelectGenres.jsx
+++ client/src/components/SelectGenres.jsx
@@ -1,6 +1,7 @@
 // toggleGenre() => adds or removes genres from the selectedGenres
 // saveGenres() => saves the user’s selected genres to DB
 import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
 
 export default function SelectGenres({ genres }) {
 
@@ -9,44 +10,44 @@ export default function SelectGenres({ genres }) {
   //console.log(genres);
 
 
-  const handleGenreSelect = (genres) => {
-    setSelectedGenres(genres);
-  }
-  const loadGenres = () => {
-    fetch("http://localhost:5001/db/genres")
-      .then((response) => response.json())
-      .then((genres) => {
-        setSelectedGenres(genres);
-      })
-  }
+  // const handleGenreSelect = (genres) => {
+  //   setSelectedGenres(genres);
+  // }
+  // const loadGenres = () => {
+  //   fetch("http://localhost:5001/db/genres")
+  //     .then((response) => response.json())
+  //     .then((genres) => {
+  //       setSelectedGenres(genres);
+  //     })
+  // }
 
-  useEffect(() => {
-    loadGenres();
-  }, []);
+  // useEffect(() => {
+  //   loadGenres();
+  // }, []);
 
-  // // populate options with genres in genres table
-  // const options = [
-  //   { value: "action", label: "Action" },
-  //   { value: "adventure", label: "Adventure" },
-  //   { value: "animation", label: "Animation" },
-  //   { value: "crime", label: "Crime" },
-  //   { value: "documentary", label: "Documentary" },
-  //   { value: "drama", label: "Drama" },
-  //   { value: "family", label: "Family"},
-  //   { value: "fantasy", label: "Fantasy" },
-  //   { value: "history", label: "History" },
-  //   { value: "horror", label: "Horror" },
-  //   { value: "music", label: "Music" },
-  //   { value: "mystery", label: "Mystery" },
-  //   { value: "news", label: "News" },
-  //   { value: "reality", label: "Reality" },
-  //   { value: "romance", label: "Romance" },
-  //   { value: "scifi", label: "Science Fiction" },
-  //   { value: "talk", label: "Talk Show" },
-  //   { value: "thriller", label: "Thriller" },
-  //   { value: "war", label: "War" },
-  //   { value: "western", label: "Western" }
-  // ]
+  // populate options with genres in genres table
+  const options = [
+    { value: "action", label: "Action" },
+    { value: "adventure", label: "Adventure" },
+    { value: "animation", label: "Animation" },
+    { value: "crime", label: "Crime" },
+    { value: "documentary", label: "Documentary" },
+    { value: "drama", label: "Drama" },
+    { value: "family", label: "Family"},
+    { value: "fantasy", label: "Fantasy" },
+    { value: "history", label: "History" },
+    { value: "horror", label: "Horror" },
+    { value: "music", label: "Music" },
+    { value: "mystery", label: "Mystery" },
+    { value: "news", label: "News" },
+    { value: "reality", label: "Reality" },
+    { value: "romance", label: "Romance" },
+    { value: "scifi", label: "Science Fiction" },
+    { value: "talk", label: "Talk Show" },
+    { value: "thriller", label: "Thriller" },
+    { value: "war", label: "War" },
+    { value: "western", label: "Western" }
+  ]
 
 
 
@@ -54,11 +55,16 @@ export default function SelectGenres({ genres }) {
     <>
       <h2>What kind of mood are you in?</h2>
       <div className="select-genre">
-      <select multiple={true} onChange={(event) => setSelectedGenres(event.target.value)}>
+        {/* tries to populate select with genres from DB - shows as undefined */}
+      {/* <select multiple={true} onChange={(event) => setSelectedGenres(event.target.value)}>
                 {genres.map((genre) => (
                     <option key={genre.genre_id} value={genre.genre_name}>{genre.genre_name}</option> 
                 ))}
-            </select>
+            </select> */}
+
+        {/* uses hardcoded genre options */}
+        <Select options={options} isMulti/>
+
         {/* button will save selected genres and fetch movies matching those genres */}
         <button type="button">Generate Movie</button>
       </div>

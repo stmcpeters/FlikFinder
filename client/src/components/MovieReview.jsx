2b4303e8ca38409eda4b20de98315207f9375351
stmcpeters <stmcpeters@gmail.com>
2024-10-28 15:22:44 -0700

    displays 10 most recent movie reviews posted

--- client/src/components/MovieReview.jsx
+++ client/src/components/MovieReview.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function MovieReview({ reviews }) {
+
+export default function MovieReview() {
+
+  const [recents, setRecents] = useState([]);
+
+  useEffect(() => {
+    const getRecents = async () => {
+      try {
+        const response = await fetch('http://localhost:5001/db/reviews/recent');
+
+        if(!response.ok) throw new Error('Network response is not ok </3');
+
+        const data = await response.json();
+        setRecents(data);
+      } catch(error){
+        console.error('Error fetching recent reviews: ', error);
+      }
+    }
+    getRecents();
+  }, []);
 
   // submitReview() =>saves a user’s review for a specific movie to the DB
 // userReviews() => fetches all user reviews for specific movie using its movieID
 
-
   return (
     <div className="movie-review">
-      <h2>Movie Review</h2>
+      <h2>Most Recent Reviews</h2>
       <ul>
-        {reviews.map((review) => {
-          return <li key={review.id}>{review.review_body}</li>
+        {recents.map((review) => {
+          return <li key={review.id}>Posted at: {review.created_at.split('T')[0]} <br /> {review.review_body}</li>
         })}
       </ul>
     </div>
